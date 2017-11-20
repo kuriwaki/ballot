ef0b01ba07b3fafd104edd3adf9cf19673f9ae90
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-11-20 23:46:47 +0900

    start roxygen

--- R/read_SC.R
+++ R/read_SC.R
@@ -1,3 +1,12 @@
-read_EL155 <- function(path, title) {
 
+#' Read Raw ballot data (EL155)
+#'
+#' Read raw SC EL155 form
+#'
+#' @param path  path to raw EL155 file
+#' @param path name of county of the file
+
+
+read_EL155 <- function(path = "build/input/SC_2010/Allendale/EL155", cname = "Allendale") {
+  raw <-  read_file(path)
 }
