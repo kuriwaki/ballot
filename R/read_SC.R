dc0855d85d109855e103153e4472b85bf64ffc55
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-11-23 10:00:40 +0900

    wrapper

--- R/read_SC.R
+++ R/read_SC.R
@@ -1,10 +1,38 @@
+#' Wrapper from path to formatted data
+#'
+#' Use the step-by-step parsing  functions to generate all votes in a race
+#'
+#' @param paths a vector of paths of the county-directory
+#'
+read_format_EL155 <- function(paths) {
 
+  # countynames
+  cnames <- gsub("[0-9A-z/_]+/([A-z]+)", "\\1", paths)
+
+  all_race <- foreach(i = 1:length(paths), .combine = "bind_rows") %do% {
+
+    rows <- read_EL155(path = glue("{paths[i]}/EL155"), cname = cnames[i])
+
+    pkey <- get_precinct_range(rows)
+    wprecinct <- add_precinct(rows, pkey)
+    rm(rows)
+
+    parsed_list <- parse_EL155(votes = wprecinct)
+    parsed_df <- list_to_df(parsed_list, wprecinct)
+    wvoter <- identify_voter(parsed_df)
+
+    wvoter
+  }
+
+  all_race
+}
 
 #' Read a EL155 file
 #'
 #' @param path path to raw file
 #' @param cname county name
 #'
+#' @import tibble readr stringr glue
 read_EL155 <- function(path = "build/input/SC_2010/Allendale/EL155",
                        cname = "Allendale") {
 
@@ -13,7 +41,6 @@ read_EL155 <- function(path = "build/input/SC_2010/Allendale/EL155",
            county = cname) %>%
     select(id, county, text)
 
-  cat(glue("read county {cname}; "))
 
 
   # weird encoding
@@ -27,22 +54,20 @@ read_EL155 <- function(path = "build/input/SC_2010/Allendale/EL155",
 
   # extract info before dropping metadata
   eID <- valid %>% filter(grepl("RUN DATE", text)) %>% distinct(text) %>%
-    mutate(election_ID = gsub(".*ID: ([0-9]+)$", "\\1", text)) %>%
+    mutate(election_ID = gsub(".*ID: ([0-9A-Z]+)$", "\\1", text)) %>%
     distinct(election_ID) %>%
     unlist()
 
-  cat(glue("county and election date: {eID};   "))
 
   n_precincts <- nonempty %>% filter(grepl("CAND VOTES", text)) %>% nrow()
-  cat(glue("Identified {n_precincts} precincts;  "))
+  cat(glue("{cname} code {eID} - with {n_precincts} precincts; "))
 
-  nototals <- nonempty %>%
+  df <- nonempty %>%
     filter(!grepl("^[\\s\\d]+$", text, perl = TRUE)) %>%
     filter(!grepl("CAND VOTES", text)) %>%
     filter(!grepl("PRECINCT TOTALS", text))
-  nrow(nototals)
 
-  nototals
+  df
 }
 
 
@@ -58,6 +83,7 @@ read_EL155 <- function(path = "build/input/SC_2010/Allendale/EL155",
 #' is the name portion of the precinct header. `precinct_id` is the ID of
 #' precinct within the county
 #'
+#' @import dplyr
 get_precinct_range <- function(df) {
   pfirst <- df %>%
     filter(grepl("RUN DATE", text, perl = TRUE)) %>%
@@ -65,7 +91,7 @@ get_precinct_range <- function(df) {
 
   pfirst_range <- pfirst %>%
     mutate(p_name = gsub(".*(?=PRECINCT)", "", text, perl = TRUE),
-           p_name = gsub("\\s+ELECTION ID: [0-9]+", "", p_name, perl = TRUE),
+           p_name = gsub("\\s+ELECTION ID: [0-9A-Z]+", "", p_name, perl = TRUE),
            p_name = gsub("\\s+", " ", p_name, perl = TRUE)) %>%
     mutate(precinct_id = 1:n(),
            p_start_id = id + 1,
@@ -78,7 +104,6 @@ get_precinct_range <- function(df) {
   pfirst_range
 }
 
-pkey <- get_precinct_range(nototals)
 
 
 #' Add precinct to each row of vote dataset, and remove other headers
@@ -86,10 +111,10 @@ pkey <- get_precinct_range(nototals)
 #' @param votes EL155 dataset of votes, product of read_EL155()
 #' @param pkey precinct key, product of get_precinct_range()
 #'
+#' @import dplyr data.table
 add_precinct <- function(votes, pkey) {
 
   # do a join-by-range with data.table
-  require(data.table)
   setDT(votes)
   setDT(pkey)
 
@@ -112,7 +137,6 @@ add_precinct <- function(votes, pkey) {
   wprecinct
 }
 
-wprecinct <- add_precinct(nototals, pkey)
 
 #' separate vote rows in to columns
 #'
@@ -126,28 +150,52 @@ wprecinct <- add_precinct(nototals, pkey)
 #' @param start_pos numerical vector of start char positions
 #' @param end_pos numerical vector of end char positions
 #'
-parse_EL155 <- function(votes, vote_col = "text", start_pos, end_pos, names_pos) {
-  start_pos = c(1,  9, 14, 16, 21, 61)
-  end_pos =   c(7, 12, 14, 19, 59, 131)
-  names_pos = c("machine_id", "ballot_style", "marker", "cand_id", "cand_name", "race")
-
-  parse_name <- function(vec, start_pos, end_pos, names_pos) {
-    parsed <- str_sub(vec, start_pos, end_pos)
-    tbl <- as_tibble(t(parsed)) %>% mutate_all(str_trim)
-    colnames(tbl) <- names_pos
-    tbl
-  }
-
-  foo %>%
-    map_dfr(~ parse_name(.1$text,
-                         start_pos = start_pos,
-                         end_pos = end_pos,
-                         names_pos = names_pos))
-
-  foo %>%
-    mutate_all()
-  parse_name(.1, start_pos = start_pos, end_pos = end_pos, names_pos = names_pos))
+#' @import stringr purrr
+parse_EL155 <- function(votes, vote_col = "text",
+                        start_pos = c(1,  9, 14, 16, 21, 61),
+                        end_pos = c(7, 12, 14, 19, 59, 131)) {
+  map(votes[[vote_col]], ~ str_sub(., start_pos, end_pos))
+}
 
 
+#' Change list of row-wise vectors to df
+#'
+#' Note: time consuming
+#'
+#' @param lst list, each item is a row
+#' @param votes data frame of votes
 
+list_to_df <- function(lst, votes) {
+
+  # machine_id = str_trim(map_chr(lst, 1)),
+  # ballot_style = str_trim(map_chr(lst, 2)),
+
+  tbl <- tibble(marker = map_chr(lst, 3),
+                cand_id = map_chr(lst, 4),
+                cand_name = map_chr(lst, 5),
+                race = map_chr(lst, 6))
+
+  # trim
+  tbl <- mutate_all(tbl, str_trim)
+
+  # change marker to numeric
+  tbl <- tbl %>%
+    mutate(voter_top = as.numeric(marker == "*")) %>%
+    select(-marker)
+
+  bind_cols(select(votes, -text),
+            tbl)
+}
+
+#' assign individual voter ID by reading the asterisk mark
+#'
+#' Use cumsum to find jumps in the asterisk
+#'
+#' @param df a data frame where each row is a birth. The column `voter_top`
+#' is 1 if the entry is the first vote on the ballot and 0 otherwise.
+identify_voter <- function(df) {
+  df %>%
+    mutate(voter_id = cumsum(voter_top)) %>%
+    select(-voter_top) %>%
+    select(county, voter_id, everything())
 }
