59d1c6d7e116de50979f6667ef4300e3e7999bbe
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-03-10 17:48:03 -0500

    add county data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Information about South Carolina counties
+#'
+#' Some district information at the county level
+#'
+#' @format
+#' \describe{
+#'   \item{county}{name of the county, in sentence case (Aiken, Anderson)}
+#'   \item{sol_race_code}{weight of the diamond, in carats}
+#'   ...
+#' }
+"sc_counties"
